ae673a16fb17a29fa10f6156c4bc878172628143
vataaly <zsolt4205@gmail.com>
2026-04-11 01:06:22 +0200

    jo67

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CookieBanner from "./components/CookieBanner";
@@ -14,29 +13,28 @@ import Impresszum from "./pages/Impresszum";
 import Adatvedelem from "./pages/Adatvedelem";
 import Cookie from "./pages/Cookie";
 
+// Ha van itt import "./App.css"; sor, azt TÖRÖLD KI!
+
 export default function App() {
   return (
     <Router>
-
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-
-        
-        <Route path="/impresszum" element={<Impresszum />} />
-        <Route path="/adatvedelem" element={<Adatvedelem />} />
-        <Route path="/cookie" element={<Cookie />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/impresszum" element={<Impresszum />} />
+          <Route path="/adatvedelem" element={<Adatvedelem />} />
+          <Route path="/cookie" element={<Cookie />} />
+        </Routes>
+      </main>
 
       <Footer />
       <CookieBanner />
-
     </Router>
   );
 }
-
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// 1. Bootstrap betöltése
 import "bootstrap/dist/css/bootstrap.min.css";
+// 2. Saját stílusok betöltése (fontos a sorrend!)
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
